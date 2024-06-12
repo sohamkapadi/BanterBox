c873e4897dc7b46f77bfe51086b921dac70650ac
Soham Kapadi <kapadisoham96@gmail.com>
2024-06-12 23:32:40 +0530

    4th part completed-socket.io implementation

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -9,6 +9,7 @@ const Message=({message})=>{
     const profilePic=fromMe ? authUser.profilePic : selectedConversation?.profilePic;
     const bubbleBgColor= fromMe ? "bg-blue-500" : "";
     const formattedTime=extractTime(message.createdAt);
+    const shakeClass=message.shouldShake ? "shake" : "";
 
     return (
         <div className={`chat ${chatClassName}`}>
@@ -19,7 +20,7 @@ const Message=({message})=>{
                     />
                 </div>
             </div>
-            <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>{message.message}</div>
+            <div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
             <div className="chat-footer opacity-50 text-xs text-white font-semibold flex gap-1 items-center">{formattedTime}</div>
         </div>
     );
